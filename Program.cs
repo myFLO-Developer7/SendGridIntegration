841773da465e03aea2b0b4e3abfd5958b29f274e
KBalane <john.kenneth_balane@questronix.com.ph>
2025-08-13 08:39:06 +0800

    Added Db Implementation

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
 ﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using SendGridIntegration;
+using System.Data;
+
 class Program
 {
-    static void Main(string[] args)
+    static async void Main(string[] args)
     {
         try
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory) // so it looks in the executable's folder
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            string? dbServer = config["ConnectionStrings:WrapperDatabase"];
+            string? systemId = config["ConnectionStrings:System_ID"];
+            if (string.IsNullOrEmpty(dbServer) || string.IsNullOrEmpty(systemId))
+            {
+                throw new Exception("Missing WrapperDatabase connection string or System ID in appsettings.json");
+            }
             if (args.Length > 0)
             {
+                SqlServer wrapperDatabase = new SqlServer();
+                DbSettings dbSettings = wrapperDatabase.GetDbSettingsFromWrapper(systemId, dbServer);
+                SqlServer database = new SqlServer(dbSettings);
                 var command = args[0];
                 if (command == "Test")
                 {
-                    Console.WriteLine("Test");
+                    DataTable dt = await database.GetData("SELECT * FROM dRegisteredUsers WHERE regUserID = 194", new List<SqlParameter>() { });
                 }
                 else
                 {
